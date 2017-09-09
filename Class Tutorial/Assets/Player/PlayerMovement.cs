effdecfd784a06d564c171700e80f0c6be937c10
Spazdude2 <spaziani96@gmail.com>
2017-09-09 00:31:33 -0400

    Basic FPS Controller
    need to add a little more, such as jump, max and min clamp on y axis for
    camera, and utilizing the "gun" or "arm". also, need to add a run speed.

---
+++ Class Tutorial/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float Speed;
+	public float sensitivity;
+	CharacterController Player;
+	public GameObject eyes;
+
+	float MoveFB;
+	float MoveLR;
+
+	float rotX;
+	float rotY;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Player = GetComponent<CharacterController> ();
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		MoveFB = Input.GetAxis ("Vertical") * Speed;
+		MoveLR = Input.GetAxis ("Horizontal") * Speed;
+
+		rotX = Input.GetAxis ("Mouse X") * sensitivity;
+		rotY = Input.GetAxis ("Mouse Y") * -sensitivity;
+
+		Vector3 Movement = new Vector3 (MoveLR, 0, MoveFB);
+		transform.Rotate (0, rotX, 0);
+		eyes.transform.Rotate (rotY, 0, 0);
+
+		Movement = transform.rotation * Movement;
+		Player.Move (Movement * Time.deltaTime);
+
+		if (Input.GetKeyDown("escape"))
+		{
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+	}
+}
